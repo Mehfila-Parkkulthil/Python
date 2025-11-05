fcb2fc96694d8483200a0e4dbe12b0c640678b88
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-05 19:08:55 +0530

    modified files

---
+++ Question-5.py
@@ -0,0 +1,12 @@
+#Question5
+#Create a program to calculate modulus(%)
+
+print("    ")
+print("Hi, lets calculate Modulus")
+print("---------------------------")
+print("Enter your number")
+number=int(input())
+print("Enter your divisor")
+divisor=int(input())
+modulus=number%divisor
+print("So reminder of " , number  , "%" , divisor , "is :" , modulus)
