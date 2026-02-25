9cab8a88bbdab7a4b27dae6193009602b3d6ca3a
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 03:01:04 +0530

    added files

--- Question-6.py
+++ Question-6.py
@@ -1,2 +1,8 @@
 #Question 6
-#
+#Print
+
+print('c:\docs\navin')
+print('\n')
+#raw string - adding r before the printing sentence , kindoff
+#saying no need to convert the string
+print(r'c:\docs\navin')
