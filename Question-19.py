667d6eafccabbff99f66008c94529b0a0a8ed17e
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 19:40:15 +0530

    changed files

--- Question-19.py
+++ Question-19.py
@@ -1,1 +1,12 @@
-#
+#Question 19
+#conditional statements
+
+light = "green"
+if(light=="green"):
+  print("Green signal")
+elif(light=="yellow"):
+  print("Yellow signal")
+elif(light=="red"):
+  print("Red signal")
+else:
+  print("Signal is broke")
