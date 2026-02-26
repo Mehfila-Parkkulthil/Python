3d80c43a68e78b1f7caade8482048274fbe2c5e0
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 17:42:23 +0530

    edited first question

--- Question-1.py
+++ Question-1.py
@@ -1,4 +1,11 @@
 # Question 1
-#Print Hello world
+#Print Hello world and some numbers
 
 print("Hello world")
+print(23)
+print("Sum of 23+43 :" , 23+43)
+print("Sum of 90-34 :", 90-34)
+print(12*3)
+print(12/6)
+print(12//6)
+print(13%5)
