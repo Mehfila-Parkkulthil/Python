0beeec881e611e2cd88a8320ac5d6698f57dcdc7
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 14:22:17 +0530

    added files

---
+++ Question-11.py
@@ -0,0 +1,10 @@
+#Question 11
+#Sets are unhasable 
+
+collection =set()
+collection.add(5)
+collection.add(6)
+collection.add("education")
+collection.add((1,2,3,4))
+#collection.add([9,34]) #shows error bcoz lists mutable
+print(collection)
