ee8a13347ec74897b2a663106b29c6c37e186fac
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 21:38:05 +0530

    changed files

--- Question-23.py
+++ Question-23.py
@@ -1,2 +1,9 @@
 #Question 23
 #Write a program to find the greatest of 3 numbers entered by the user
+
+
+first = int(input("Enter your first number:"))
+second = int(input("Enter your second number:"))
+third =int(input("Enter your third number:"))
+greatest = max(first,second, third)
+print(greatest, "is the greatesst number")
