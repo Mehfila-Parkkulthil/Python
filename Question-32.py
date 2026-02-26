e11ae8e52ecf9f6bc02adb6fa208d3f3be9980b8
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-27 01:39:16 +0530

    added to new files

---
+++ Question-32.py
@@ -0,0 +1,7 @@
+# Question 32
+#Write a program to count the number of students with A grade in the following tuple
+
+list=["C","A","D","A","F","B","A","A","A","C","D"]
+count= list.count("A")
+print(list)
+print(count)
