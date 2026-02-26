86bd6abc3e8e1ed71692b8d56407d94edd26df05
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 13:40:47 +0530

    added files

---
+++ Question-9.py
@@ -0,0 +1,12 @@
+#Question 9
+#String concatenation
+
+str1="This is a string"
+str2='''This is Mehfila'''
+str3='I love coding'
+
+print(str1 + ' ' + str3) #added space
+print(len(str1))
+len2=len(str2)
+print(len2)
+print(str1[5])
