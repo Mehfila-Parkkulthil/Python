3e74c72ea6a1bc5ee9e5af5b085f6984b52046af
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-27 05:26:36 +0530

    added to new files

--- Question-38.py
+++ Question-38.py
@@ -1,2 +1,14 @@
 #Question 38
-#Sets in python
+#Store the following words and meanings in python dictionary
+
+dictionary_of_python={
+  "table" : {
+              "a piece of furniture",
+              "list of facts and figures"
+            },
+  "cat" : "a small animal"
+}
+
+print(dictionary_of_python)
+print(dictionary_of_python["cat"])
+print(dictionary_of_python["table"])
