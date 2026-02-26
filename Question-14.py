a0c45d9bf17cfced9a04a57dd5c5bf1dcd80d450
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 18:15:48 +0530

    changed files

---
+++ Question-14.py
@@ -0,0 +1,14 @@
+#Question 14
+#Indexing -1 
+
+str ="Apna_College is a good youtube channel"
+# for i in range(38): #starts at 0 and stops before 38
+#   print("str","[",i,"]")
+for i in range(39):
+   print(f"str[{i}]")
+   #both gives same
+   print()
+for i in range(1,39):
+  print(f"str[{i}]")
+  #f-string means formatted string .
+  #f"..." lets you insert variables inside{}
