30114635e1e737495aa3526e3925227fa28bc647
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-27 00:03:26 +0530

    changed files

--- Question-25.py
+++ Question-25.py
@@ -2,7 +2,20 @@
 #Strings are immutable
 
 str="hello"
-print(str[])
-str[0]="y"
-print(str[])
+print(str)
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+print()
+print(str[-1])
+print(str[-2])
+print(str[-3])
+print(str[-4])
+print(str[-5])
+
+# print(str[5])-shows error as doesnt exist
+# str[0]="y"
+# print(str[])
 #shows error becoz strings are immutable
