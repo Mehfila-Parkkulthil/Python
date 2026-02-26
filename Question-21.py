14553f1c0e27d92b6c0a2272a54be1a594c51740
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 21:26:12 +0530

    changed files

---
+++ Question-21.py
@@ -0,0 +1,10 @@
+#Question 21
+#Write a program to check if a number entered by the user is even or odd
+
+number=int(input("Enter your number:"))
+if(number % 2 ==0):
+  print("Its even")
+else:
+  print("Its odd")
+
+  
