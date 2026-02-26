e8af7071f75c394f4579a0d5f75d76b6c1f3f538
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 18:06:17 +0530

    changed files

---
+++ Question-12.py
@@ -0,0 +1,13 @@
+#Question 12
+#Inputs in python
+
+#String input
+name = input("name :")
+print("Hi " + name)
+#Int input
+age = int(input("age:"))
+print("So you are " , age , "years old") #you cant concatenate str with int but use commas
+#float input
+Price_of_your_pen = float(input("price of your pen :"))
+print("So your pen costs " , Price_of_your_pen , "Rupees.")
+
---
+++ Question-13.py
@@ -0,0 +1,20 @@
+#Question 13
+#Type conversion
+
+#Casting means manually this is the type of data type, means automatically pyton will identify which datatype it is.
+
+#Eg:
+a=2
+b=4.32
+sum = a+b
+print(type(a))
+print(type(b))
+print (sum)#solution will be in float automatically as float is bigger than int
+
+c="2"
+d=4.34
+#here a is a string and b is float
+print(type(a))
+print(type(b))
+sum_1=c+d
+print(sum_1)#shows error not possible to add with a string and float
