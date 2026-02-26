04277fd750283df82a5943a9d20af3b9c81bf88d
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-02-26 19:17:20 +0530

    changed files

---
+++ Question-16.py
@@ -0,0 +1,14 @@
+#Question 16
+#Slicing
+
+str="Mehfila parkkulthil"
+print("so length of the string is : ",len(str))
+print(str[1:4])
+print(str[:4])
+print(str[3:])
+print(str[1:len(str)])
+print(str[-3])
+print(str[-2])
+print(str[-8])
+print(str[-15:-2])
+print(str[-6:12]) #wont work as its reverse direction
